318f3c0d5645238092583918240863269a10dda2
Иван <ghostprizrak2011@gmail.com>
2025-03-21 21:08:03 +1000

    Add files via upload
    код шампуня
    
    Signed-off-by: Иван <ghostprizrak2011@gmail.com>

---
+++ Shampoo.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "         ___" << std::endl;
+    std::cout << "        |   |" << std::endl;
+    std::cout << "     ___|___|___" << std::endl;
+    std::cout << "    /           \\" << std::endl;
+    std::cout << "   /             \\" << std::endl;
+    std::cout << "  /               \\" << std::endl;
+    std::cout << " /                 \\" << std::endl;
+    std::cout << "|                   |" << std::endl;
+    std::cout << "|      SHAMPOO      |" << std::endl;
+    std::cout << "|                   |" << std::endl;
+    std::cout << " \\                 /" << std::endl;
+    std::cout << "  \\               /" << std::endl;
+    std::cout << "   \\             /" << std::endl;
+    std::cout << "    \\___________/" << std::endl;
+    return 0;
+}
